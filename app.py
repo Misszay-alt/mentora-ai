d28d2f14dfefdb875af5333a72e610f72cab7436
Misszay-alt <euniceosei574@gmail.com>
2026-07-05 14:58:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ for filename in os.listdir(folder_path):
                     text += para.text + "\n"
 
             all_text += f"\n\n--- START OF {filename} ---\n{text}\n--- END OF {filename} ---\n"
-        except Exception as e:
-            all_text += f"\nError reading {filename}: {e}"
-    return all_text if all_text else "No documents loaded."
+   except Exception as e:
+    all_text += f"\nError reading {filename}: {e}"
 
+return all_text if all_text else "No documents loaded."
   st.header("1. Course Materials")
 st.info("Your notes are already loaded by your teacher")
 st.success("Mentora is ready! Ask me anything.")
